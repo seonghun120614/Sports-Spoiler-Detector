f3d40ade4c8b6e15309cdbe6555686498910a3ec
seonghun120614 <seonghun120614@gmail.com>
2026-06-28 15:42:25 +0900

    chore: Model unload on test mode

--- src/core/lifespan.py
+++ src/core/lifespan.py
@@ -8,13 +8,11 @@ import os
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    try:
+    if not "TEST_FLAG" in os.environ:
         app.state.ner = GliNER()
         app.state.object_detector = GroundingDINO()
         app.state.text_classifier = SetFitImpl()
         app.state.emotion_recognition = DeepFaceRecognition()
         app.state.pose_detector = YoloV8Pose()
-    except Exception as e:
-        logger.error("모델 로딩 실패 — TEST 모드로 전환합니다")
-        os.environ["TEST_FLAG"] = "1"
-    yield
+
+    logger.error("[TEST 모드]")
