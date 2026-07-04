6a82307e0d0bb33cf2f1d739c19862ad1fdf2ee7
seonghun120614 <seonghun120614@gmail.com>
2026-07-04 14:14:23 +0900

    chore: Syncronize with main

--- src/api/v1/endpoints/constants.py
+++ src/api/v1/endpoints/constants.py
@@ -1,177 +1,191 @@
-_TEST_RESPONSE_DATA = {
-    "blurred_video": {
-        "video_id": "TkCLtWqju7g",
-        "blurred_image": {
-            "width": 320,
-            "height": 180,
-            "objects": [
-                {
-                    "label": "player",
-                    "confidence": 0.476,
-                    "box": [
-                        127.69,
-                        2.8,
-                        318.83,
-                        178.71
-                    ]
-                },
-                {
-                    "label": "player",
-                    "confidence": 0.328,
-                    "box": [
-                        0.83,
-                        86.1,
-                        71.05,
-                        179.27
-                    ]
-                }
-            ],
-            "faces": [
-                {
-                    "label": "human face face",
-                    "confidence": 0.444,
-                    "box": [
-                        188.8,
-                        26.2,
-                        251.73,
-                        110.03
-                    ],
-                    "emotion": {
-                        "angry": 84.7,
-                        "disgust": 7.48,
-                        "fear": 4.57,
-                        "happy": 0.0,
-                        "sad": 3.22,
-                        "surprise": 0.0,
-                        "neutral": 0.03
-                    },
-                    "dominant_emotion": "angry",
-                    "region": {
-                        "x": 0,
-                        "y": 1,
-                        "w": 62,
-                        "h": 71,
-                        "left_eye": None,
-                        "right_eye": None
-                    },
-                    "face_confidence": 0.95
-                }
-            ],
-            "angles": [
-                {
-                    "angles": {
-                        "left_arm": 56.92,
-                        "right_arm": 48.37,
-                        "left_knee": 34.84,
-                        "right_knee": 91.21
-                    },
-                    "flags": {
-                        "arms_wide_open": False,
-                        "knees_sliding": True,
-                        "is_celebration_matched": False
-                    }
-                },
-                {
-                    "angles": {
-                        "left_arm": 75.93,
-                        "right_arm": 20.96,
-                        "left_knee": 11.74,
-                        "right_knee": 115.9
-                    },
-                    "flags": {
-                        "arms_wide_open": False,
-                        "knees_sliding": True,
-                        "is_celebration_matched": False
-                    }
-                },
-                {
-                    "angles": {
-                        "left_arm": 59.07,
-                        "right_arm": 29.2,
-                        "left_knee": 60.98,
-                        "right_knee": 81.78
-                    },
-                    "flags": {
-                        "arms_wide_open": False,
-                        "knees_sliding": False,
-                        "is_celebration_matched": False
-                    }
-                }
-            ],
-            "overlay_texts": [
-                {
-                    "box": [
-                        [
-                            13,
-                            39
-                        ],
-                        [
-                            129,
-                            39
-                        ],
-                        [
-                            129,
-                            99
-                        ],
-                        [
-                            13,
-                            99
-                        ]
-                    ],
-                    "text": "2:1",
-                    "ocr_confidence": 0.9909533629251359,
-                    "spoiler": {
-                        "entities": {
-                            "success": [],
-                            "failure": [],
-                            "draw": [],
-                            "round": [],
-                            "name": [],
-                            "scoring_text": [
-                                {
-                                    "text": "1",
-                                    "confidence": 0.9422452449798584,
-                                    "start": 2,
-                                    "end": 3
-                                }
-                            ],
-                            "special_event": [],
-                            "emotive": [],
-                            "aftermath": []
+_TEST_RESPONSE_DATA = [
+    {
+        "blurred_video": {
+            "video_id": "UXZPxz6H_kU",
+            "blurred_image": {
+                "width": 320,
+                "height": 180,
+                "objects": [],
+                "faces": [
+                    {
+                        "label": "happy",
+                        "confidence": 0.86,
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 0,
+                                "y": 0
+                            },
+                            "bottom_right": {
+                                "x": 319,
+                                "y": 179
+                            }
                         }
                     }
-                }
-            ]
-        },
-        "blurred_text": {
-            "text": "[속보] 변변한 슈팅도 없이 무력했던 90분, 축구대표팀 남아공에 0-1 '충격패'…32강도 '불투명', 이번에도 '경우의 수' /2026년 6월 25일(목)/KBS",
-            "spoiler": "Direct Spoiler",
-            "spans": {
-                "2": [],
-                "3": [
+                ],
+                "angles": [],
+                "overlay_texts": [
                     {
-                        "text": "축구대표팀",
-                        "confidence": 0.9157509803771973,
-                        "start": 26,
-                        "end": 31
+                        "label": None,
+                        "confidence": 0.0,
+                        "text": "",
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 80,
+                                "y": 0
+                            },
+                            "bottom_right": {
+                                "x": 113,
+                                "y": 11
+                            }
+                        },
+                        "span": None
                     },
                     {
-                        "text": "0-1",
-                        "confidence": 0.9999654293060303,
-                        "start": 37,
-                        "end": 40
-                    }
-                ],
-                "4": [
+                        "label": "name",
+                        "confidence": 0.9532710313796997,
+                        "text": "XCC",
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 269,
+                                "y": 12
+                            },
+                            "bottom_right": {
+                                "x": 306,
+                                "y": 32
+                            }
+                        },
+                        "span": {
+                            "start": 0,
+                            "end": 3
+                        }
+                    },
                     {
-                        "text": "불투명",
-                        "confidence": 0.5920975804328918,
-                        "start": 53,
-                        "end": 56
+                        "label": None,
+                        "confidence": 0.99,
+                        "text": "sports",
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 270,
+                                "y": 30
+                            },
+                            "bottom_right": {
+                                "x": 306,
+                                "y": 43
+                            }
+                        },
+                        "span": None
+                    },
+                    {
+                        "label": None,
+                        "confidence": 0.72,
+                        "text": "[3분 하이라이트]",
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 12,
+                                "y": 91
+                            },
+                            "bottom_right": {
+                                "x": 204,
+                                "y": 124
+                            }
+                        },
+                        "span": None
+                    },
+                    {
+                        "label": "name",
+                        "confidence": 1.0,
+                        "text": "스페인 VS 오스트리아",
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 11,
+                                "y": 119
+                            },
+                            "bottom_right": {
+                                "x": 258,
+                                "y": 153
+                            }
+                        },
+                        "span": {
+                            "start": 7,
+                            "end": 12
+                        }
+                    },
+                    {
+                        "label": "name",
+                        "confidence": 0.9998618364334106,
+                        "text": "스페인 VS 오스트리아",
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 11,
+                                "y": 119
+                            },
+                            "bottom_right": {
+                                "x": 258,
+                                "y": 153
+                            }
+                        },
+                        "span": {
+                            "start": 0,
+                            "end": 3
+                        }
+                    },
+                    {
+                        "label": None,
+                        "confidence": 0.35,
+                        "text": "2026 FIFA 북숨미 월드러",
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 8,
+                                "y": 163
+                            },
+                            "bottom_right": {
+                                "x": 123,
+                                "y": 178
+                            }
+                        },
+                        "span": None
+                    },
+                    {
+                        "label": None,
+                        "confidence": 0.97,
+                        "text": "32감전",
+                        "bounding_box": {
+                            "top_left": {
+                                "x": 128,
+                                "y": 163
+                            },
+                            "bottom_right": {
+                                "x": 164,
+                                "y": 178
+                            }
+                        },
+                        "span": None
                     }
                 ]
+            },
+            "blurred_text": {
+                "text": "[3분 하이라이트] 32강 스페인 VS 오스트리아｜2026 FIFA 북중미 월드컵",
+                "spoiler": "Direct Spoiler",
+                "spans": {
+                    "3": [
+                        {
+                            "label": "name",
+                            "text": "32강 스페인",
+                            "confidence": 0.9999784231185913,
+                            "start": 11,
+                            "end": 18
+                        },
+                        {
+                            "label": "name",
+                            "text": "오스트리아",
+                            "confidence": 0.9999507665634155,
+                            "start": 22,
+                            "end": 27
+                        }
+                    ]
+                }
             }
-        }
-    },
-    "timestamp": "2026-06-29T17:55:49.748850"
-}
+        },
+        "timestamp": "2026-07-04T05:10:12.998645"
+    }
+]
